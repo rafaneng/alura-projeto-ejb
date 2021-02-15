66979a2edfe615da4a703e76b940dde3c227bbc2
rafaneng <rafaneng@gmail.com>
2021-02-15 19:57:07 -0300

    first commit

---
+++ src/main/java/br/com/alura/servico/AgendamentoEmailServico.java
@@ -0,0 +1,13 @@
+package br.com.alura.servico;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class AgendamentoEmailServico {
+
+	public List<String> listar() {
+		return List.of("joao@alura.com.br");
+	}
+}
