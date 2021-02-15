37086fc8c0e121923a1879eecb9056df16d6bc5d
rafaneng <rafaneng@gmail.com>
2021-02-15 20:16:09 -0300

    Trabalhando com banco de dados usando JPA

---
+++ src/main/java/br/com/alura/dao/AgendamentoEmailDAO.java
@@ -0,0 +1,21 @@
+package br.com.alura.dao;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.com.alura.entidade.AgendamentoEmail;
+
+@Stateless
+public class AgendamentoEmailDAO {
+
+	@PersistenceContext
+	EntityManager entityManager;
+
+	public List<AgendamentoEmail> listar() {
+		return entityManager.createQuery("SELECT ae FROM AgendamentoEmail ae", AgendamentoEmail.class).getResultList();
+	}
+
+}
--- src/main/java/br/com/alura/servico/AgendamentoEmailServico.java
+++ src/main/java/br/com/alura/servico/AgendamentoEmailServico.java
@@ -3,11 +3,18 @@ package br.com.alura.servico;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import br.com.alura.dao.AgendamentoEmailDAO;
+import br.com.alura.entidade.AgendamentoEmail;
 
 @Stateless
 public class AgendamentoEmailServico {
+	
+	@Inject
+	private AgendamentoEmailDAO dao;
 
-	public List<String> listar() {
-		return List.of("joao@alura.com.br");
+	public List<AgendamentoEmail> listar() {
+		return dao.listar();
 	}
 }
