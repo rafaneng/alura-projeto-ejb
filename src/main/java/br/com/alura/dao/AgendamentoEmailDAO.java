5a207e23082399f7de15ad260b039b5d39af7eb2
rafaneng <rafaneng@gmail.com>
2021-02-19 13:29:49 -0300

    criação de job para envio de email automatico

--- src/main/java/br/com/alura/dao/AgendamentoEmailDAO.java
+++ src/main/java/br/com/alura/dao/AgendamentoEmailDAO.java
@@ -10,16 +10,25 @@ import br.com.alura.entidade.AgendamentoEmail;
 
 @Stateless
 public class AgendamentoEmailDAO {
-	
+
 	@PersistenceContext
 	private EntityManager entityManager;
-	
+
 	public List<AgendamentoEmail> listar() {
-		return entityManager.createQuery("SELECT ae FROM AgendamentoEmail ae",
-				AgendamentoEmail.class).getResultList();
+		return entityManager.createQuery("SELECT ae FROM AgendamentoEmail ae", AgendamentoEmail.class).getResultList();
 	}
 
 	public void inserir(AgendamentoEmail agendamentoEmail) {
 		entityManager.persist(agendamentoEmail);
+	}
+
+	public List<AgendamentoEmail> listarPorNaoAgendado() {
+		return entityManager
+				.createQuery("SELECT ae FROM AgendamentoEmail ae WHERE ae.agendado = false", AgendamentoEmail.class)
+				.getResultList();
+	}
+
+	public void alterar(AgendamentoEmail agendamentoEmail) {
+		entityManager.merge(agendamentoEmail);
 	}
 }
---
+++ src/main/java/br/com/alura/job/AgendamentoEmailJob.java
@@ -0,0 +1,28 @@
+package br.com.alura.job;
+
+import java.util.List;
+
+import javax.ejb.Schedule;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import br.com.alura.entidade.AgendamentoEmail;
+import br.com.alura.servico.AgendamentoEmailServico;
+
+@Stateless
+public class AgendamentoEmailJob {
+	
+	@Inject
+	private AgendamentoEmailServico agendamentoEmailServico;
+	
+	@Schedule(hour = "*", minute = "*", second = "*/10")
+	public void enviarEmail() {
+		List<AgendamentoEmail> listarPorNaoAgendado = agendamentoEmailServico.listarPorNaoAgendado();
+		listarPorNaoAgendado.forEach(emailNaoAgendado -> {
+			agendamentoEmailServico.enviar(emailNaoAgendado);
+			agendamentoEmailServico.alterar(emailNaoAgendado);
+		});
+	
+	}
+
+}
