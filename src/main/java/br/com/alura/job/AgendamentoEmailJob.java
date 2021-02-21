5e7089fbd5e3b65e1247fe35fd6315bc8dbe35fe
rafaneng <rafaneng@gmail.com>
2021-02-21 16:31:53 -0300

    criacao de Producer para envio de emails para fila

--- src/main/java/br/com/alura/job/AgendamentoEmailJob.java
+++ src/main/java/br/com/alura/job/AgendamentoEmailJob.java
@@ -2,9 +2,13 @@ package br.com.alura.job;
 
 import java.util.List;
 
+import javax.annotation.Resource;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.inject.Inject;
+import javax.jms.JMSConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.Queue;
 
 import br.com.alura.entidade.AgendamentoEmail;
 import br.com.alura.servico.AgendamentoEmailServico;
@@ -15,11 +19,18 @@ public class AgendamentoEmailJob {
 	@Inject
 	private AgendamentoEmailServico agendamentoEmailServico;
 	
+	@Inject
+	@JMSConnectionFactory("java:jboss/DefaultSJMSConnectionFactory")
+	private JMSContext context;
+	
+	@Resource(mappedName = "java:/jms/queue/EmailQueue")
+	private Queue queue;
+	
 	@Schedule(hour = "*", minute = "*", second = "*/10")
 	public void enviarEmail() {
 		List<AgendamentoEmail> listarPorNaoAgendado = agendamentoEmailServico.listarPorNaoAgendado();
 		listarPorNaoAgendado.forEach(emailNaoAgendado -> {
-			agendamentoEmailServico.enviar(emailNaoAgendado);
+			context.createProducer().send(queue, emailNaoAgendado);
 			agendamentoEmailServico.alterar(emailNaoAgendado);
 		});
 	
